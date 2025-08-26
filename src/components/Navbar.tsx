7dbe57efe3e9f1be4478f226e7d12bd413db90f6
spajicdominik <dominikspajic7@gmail.com>
2025-08-26 12:07:46 +0200

    Added navbar items and fonts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 function Navbar() {
     return (
-        <div className="w-full h-36 border-b border-[#ffffff]">
-            <div className="bg-black w-48 h-full flex items-center justify-center" >
+        <div className="w-full h-36 border-b border-[#B2B2B2] flex justify-between">
+            <div className="bg-[#232323] w-48 h-full flex items-center justify-center" >
                 <img src="src/assets/logo.png" alt="" className="w-2/3"/>
+            </div>
+
+            <div className="flex justify-evenly">
+                <div className="font-[Italiana] text-black w-28 flex items-center justify-center">HOME</div>
+                <div className="font-[Italiana] text-black w-28 flex items-center justify-center">PORTFOLIO</div>
+                <div className="font-[Italiana] text-black w-28 flex items-center justify-center">ABOUT</div>
+                <div className="font-[Italiana] text-black w-28 flex items-center justify-center">CONTACT</div>
+                <div className="font-[Italiana] text-black w-28 flex items-center justify-center">GALLERIES</div>
             </div>
         </div>
     )
