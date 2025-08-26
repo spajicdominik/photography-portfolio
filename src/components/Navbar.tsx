e04f5d50b1d5afe58778da77cea4e48a559a60e8
spajicdominik <dominikspajic7@gmail.com>
2025-08-26 11:27:20 +0200

    Started work on the navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <div className="w-full h-36 border-b border-[#ffffff]">
+            <div className="bg-black w-48 h-full flex items-center justify-center" >
+                <img src="src/assets/logo.png" alt="" className="w-2/3"/>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
