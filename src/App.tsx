5ff6a93cdd0d2bbc678c47af253b2766d7b14c63
spajicdominik <dominikspajic7@gmail.com>
2025-09-01 23:43:45 +0200

    Added minor animations to footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Preview from './components/Preview'
 import Description from './components/Description'
 import Galleries from './components/Galleries'
 import Footer from './components/Footer'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,53 @@
 import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
+import {motion, AnimatePresence} from "framer-motion"
+
 
 function Footer() {
   return (
     <div className="w-full bg-black flex flex-col items-center pt-8 mt-4">
       <div className="flex justify-between w-48 mt-4">
-        <a
+        <motion.a
+          whileHover={{scale: 1.2}}
+          whileTap={{scale:0.9}}
           href="https://instagram.com"
           className="text-white hover:text-white"
         >
           <FaInstagram size={32}/>
-        </a>
+        </motion.a>
 
-        <a
+        <motion.a
+          whileHover={{scale: 1.2}}
+          whileTap={{scale:0.9}}
           href="https://facebook.com"
           className="text-white hover:text-white"
         >
           <FaFacebook size={32}/>
-        </a>
+        </motion.a>
 
-        <a
+        <motion.a
+          whileHover={{scale: 1.2}}
+          whileTap={{scale:0.9}}
           href="https://whatsapp.com"
           className="text-white hover:text-white"
         >
           <FaWhatsapp size={32}/>
-        </a>
+        </motion.a>
       </div>
 
       <div className="flex justify-between mt-6">
-        <div className="mr-3 flex items-center justify-center">
+        <div className="mr-3 flex items-center justify-center cursor-pointer">
             <p className="text-xs ">HOME</p>
         </div>
-        <div className="mr-3 flex items-center justify-center">
+        <div className="mr-3 flex items-center justify-center cursor-pointer">
             <p className="text-xs">PORTFOLIO</p>
         </div>
-        <div className="mr-3 flex items-center justify-center">
+        <div className="mr-3 flex items-center justify-center cursor-pointer">
             <p className="text-xs">ABOUT</p>
         </div>
-        <div className="mr-3 flex items-center justify-center">
+        <div className="mr-3 flex items-center justify-center cursor-pointer">
             <p className="text-xs">CONTACT</p>
         </div>
-        <div className="mr-3 flex items-center justify-center">
+        <div className="mr-3 flex items-center justify-center cursor-pointer">
             <p className="text-xs">GALLERIES</p>
         </div>
       </div>
