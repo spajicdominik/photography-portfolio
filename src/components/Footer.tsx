092aaac245484b9129e30a0c31b7ab4ec1757982
spajicdominik <dominikspajic7@gmail.com>
2025-09-04 23:47:55 +0200

    f

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
-import {motion, AnimatePresence} from "framer-motion"
+import {motion} from "framer-motion"
 import { useNavigate } from "react-router-dom";
 
 function Footer() {
