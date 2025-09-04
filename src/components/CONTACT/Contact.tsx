ef07e26fc7261a0d52c92f180fc15d6f03f8d7a1
spajicdominik <dominikspajic7@gmail.com>
2025-09-04 23:11:37 +0200

    Update Contact.tsx

--- src/components/CONTACT/Contact.tsx
+++ src/components/CONTACT/Contact.tsx
@@ -1,8 +1,9 @@
 import { motion, AnimatePresence } from "framer-motion";
+import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <motion.div
         className="w-full relative"
         initial={{ opacity: 0, y: 30 }}
@@ -21,6 +22,68 @@ function Contact() {
           </h1>
         </div>
       </motion.div>
+
+      <div className="w-5/6 flex-col md:flex items-center justify-center">
+        <div className="">
+          <h1 className="text-4xl font-[Italiana] text-black font-extrabold mt-4 mb-10">
+            Contact Me
+          </h1>
+          <p className="text-xl font-[Italiana] text-black font-medium mb-6">
+            For general inquiries or technical information, email:{" "}
+            <a className="text-blue-600" href="">
+              petrisevacandrea0@gmail.com{" "}
+            </a>
+          </p>
+          <h2 className="text-2xl font-[Italiana] text-black font-extrabold mb-10">
+            Connect with me on Social
+          </h2>
+          <div className="flex">
+            <motion.a
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              href="https://www.instagram.com/andreapetrisevac/"
+              className="text-black hover:text-black mr-5"
+            >
+              <FaInstagram size={44} />
+            </motion.a>
+
+            <motion.a
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              href="https://www.facebook.com/andrea.petrisevac.5"
+              className="text-black hover:text-black mr-5"
+            >
+              <FaFacebook size={44} />
+            </motion.a>
+
+            <motion.a
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              href="https://whatsapp.com"
+              className="text-black hover:text-black"
+            >
+              <FaWhatsapp size={44} />
+            </motion.a>
+          </div>
+        </div>
+
+        <div className="flex flex-col mt-10">
+            <p className="text-xl font-[Italiana] text-black font-medium">Your name (required)</p>
+            <input type="text "  className="bg-white text-black w-5/6 h-14"/>
+
+            <p className="text-xl font-[Italiana] text-black font-medium">Your Email (required)</p>
+            <input type="text "  className="bg-white text-black w-5/6 h-14"/>
+
+            <p className="text-xl font-[Italiana] text-black font-medium">Subject</p>
+            <input type="text "  className="bg-white text-black w-5/6 h-14"/>
+
+            <p className="text-xl font-[Italiana] text-black font-medium">Your message</p>
+            <input type="text "  className="bg-white text-black w-5/6 h-72"/>
+
+            <button className="w-32 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Submit</button>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
