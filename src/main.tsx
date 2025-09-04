3d55e8050556a55763b6e70e33e5ec9ba0ad04be
spajicdominik <dominikspajic7@gmail.com>
2025-09-04 23:44:12 +0200

    d

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from "react-dom/client";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
